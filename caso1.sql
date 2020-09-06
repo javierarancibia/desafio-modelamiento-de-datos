80c70efac2f9c288c176aac28533baac0efee439
Javier <javier.arancibia.reyes@hotmail.com>
2020-09-05 23:28:07 -0400

    Correccion caso 1

--- caso1.sql
+++ caso1.sql
@@ -4,4 +4,4 @@ CREATE DATABASE caso1;
 CREATE TABLE trabajadores(id SERIAL PRIMARY KEY, Nombre VARCHAR(100) NOT NULL, rut VARCHAR(100) NOT NULL, direccion VARCHAR(100) NOT NULL);
 CREATE TABLE liquidaciones(id SERIAL PRIMARY KEY, sueldo_bruto FLOAT NOT NULL, cotizaciones FLOAT NOT NULL, sueldo_liquido FLOAT NOT NULL);
 CREATE TABLE departamentos(id SERIAL PRIMARY KEY, nombre VARCHAR(100) NOT NULL);
-CREATE TABLE trabajador_liquidaciones(id SERIAL PRIMARY KEY, id_trabajador INT NOT NULL, id_liquidaciones INT NOT NULL, id_departamento INT NOT NULL);
+CREATE TABLE trabajador_liquidaciones(id SERIAL PRIMARY KEY, id_trabajador INT NOT NULL, id_liquidacion INT NOT NULL, id_departamento INT NOT NULL, FOREIGN KEY(id_trabajador) REFERENCES trabajadores(id), FOREIGN KEY(id_liquidaciones) REFERENCES liquidacion(id), FOREIGN KEY(id_departamento) REFERENCES departamentos(id);
